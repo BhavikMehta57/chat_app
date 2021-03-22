482fde26b0634389fde034b24606779a1c5ee8f8
BhavikMehta57 <shadow7bm@gmail.com>
2021-03-22 19:03:13 +0530

    device info

--- chat_app/lib/widget/widget.dart
+++ chat_app/lib/widget/widget.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:device_info/device_info.dart';
+
+DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
 
 Widget appBarMain(BuildContext context) {
   return AppBar(
@@ -8,7 +11,65 @@ Widget appBarMain(BuildContext context) {
       height: 40,
     ),
     elevation: 0.0,
+    actions: [
+
+      IconButton(icon:Icon(Icons.announcement), onPressed: () {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) => _buildPopupDialog(context),
+        );},)
+    ],
     centerTitle: false,
+  );
+}
+
+Widget _buildPopupDialog(BuildContext context) {
+  return new AlertDialog(
+    title: const Text('Device Info'),
+    content: new Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        FutureBuilder<AndroidDeviceInfo>(future: deviceInfo.androidInfo,
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.hasData) {
+            AndroidDeviceInfo info = snapshot.data;
+            return Container(
+                child: Column(
+                  children: [
+                    Text("Brand: ${info.brand}"),
+                    Text("Version: ${info.version}"),
+                    Text("Board: ${info.board}"),
+                    Text("Bootloader: ${info.bootloader}"),
+                    Text("Device: ${info.device}"),
+                    Text("Display: ${info.display}"),
+                    Text("Hardware: ${info.hardware}"),
+                    Text("Host: ${info.host}"),
+                    Text("id: ${info.id}"),
+                    Text("Manufacturer: ${info.manufacturer}"),
+                    Text("Model: ${info.model}"),
+                    Text("Product: ${info.product}"),
+                    Text("Tags: ${info.tags}"),
+                    Text("Type: ${info.type}"),
+                    Text("Is Physical Device ? ${info.isPhysicalDevice}"),
+                    Text("AndroidID: ${info.androidId}"),
+                  ],
+                )
+            );
+          }
+          return CircularProgressIndicator();
+        })
+      ],
+    ),
+    actions: <Widget>[
+      new FlatButton(
+        onPressed: () {
+          Navigator.of(context).pop();
+        },
+        textColor: Theme.of(context).primaryColor,
+        child: const Text('Close'),
+      ),
+    ],
   );
 }
 
